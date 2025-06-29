3a96f851a7ac299ef8f1419a841f6c246a18e3c1
Vishal Maurya <124227173+2022ugmm081@users.noreply.github.com>
2025-06-29 18:29:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -402,5 +402,4 @@ def api_land_cover_classification():
         return json.dumps({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port=os.environ.get("PORT")
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
