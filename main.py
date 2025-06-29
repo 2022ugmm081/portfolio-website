536ad8707d45116e5649265082934772a67f14ee
Vishal Maurya <124227173+2022ugmm081@users.noreply.github.com>
2025-06-29 18:29:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from app import app
-import os
 
 if __name__ == '__main__':
-    port=os.environ.get("PORT")
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
